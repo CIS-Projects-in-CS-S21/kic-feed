bf6628263599b5c5cb8e86a0b1374c94d60e0713
regnivon <qdnovinger@gmail.com>
2021-04-04 19:15:15 -0400

    added health client wrapper, implemented post ranking

--- internal/server/clientWrappers.go
+++ internal/server/clientWrappers.go
@@ -2,13 +2,14 @@ package server
 
 import (
 	"context"
-	"fmt"
-	pbcommon "github.com/kic/feed/pkg/proto/common"
-	"google.golang.org/grpc/metadata"
+	"strconv"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 
+	pbcommon "github.com/kic/feed/pkg/proto/common"
 	pbfriends "github.com/kic/feed/pkg/proto/friends"
+	pbhealth "github.com/kic/feed/pkg/proto/health"
 	pbmedia "github.com/kic/feed/pkg/proto/media"
 	pbusers "github.com/kic/feed/pkg/proto/users"
 )
@@ -28,7 +29,7 @@ func (f *FriendClientWrapper) GetFriendsForUser(
 	userID int64,
 	authCredentials string,
 ) ([]uint64, error) {
-	md := metadata.Pairs("Authorization", fmt.Sprintf("Bearer %v", authCredentials))
+	md := metadata.Pairs("Authorization", authCredentials)
 	ctx = metadata.NewOutgoingContext(ctx, md)
 	req := &pbfriends.GetFriendsForUserRequest{
 		User: &pbcommon.User{
@@ -44,8 +45,13 @@ func (f *FriendClientWrapper) GetFriendsForUser(
 	return resp.Friends, nil
 }
 
-func (f *FriendClientWrapper) GetConnectionBetweenUsers(ctx context.Context, uid1 int64, uid2 int64, authCredentials string) (float32, error) {
-	md := metadata.Pairs("Authorization", fmt.Sprintf("Bearer %v", authCredentials))
+func (f *FriendClientWrapper) GetConnectionBetweenUsers(
+	ctx context.Context,
+	uid1 int64,
+	uid2 int64,
+	authCredentials string,
+) (float32, error) {
+	md := metadata.Pairs("Authorization", authCredentials)
 	ctx = metadata.NewOutgoingContext(ctx, md)
 	req := &pbfriends.GetConnectionBetweenUsersRequest{
 		FirstUserID:  uint64(uid1),
@@ -71,6 +77,32 @@ func NewMediaClientWrapper(conn *grpc.ClientConn) *MediaClientWrapper {
 	}
 }
 
+func (m *MediaClientWrapper) GetFilesForUser(
+	ctx context.Context,
+	userID int64,
+	authCredentials string,
+) ([]*pbcommon.File, error) {
+	md := metadata.Pairs("Authorization", authCredentials)
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	fileMetadata := make(map[string]string)
+
+	fileMetadata["userID"] = strconv.FormatInt(userID, 10)
+
+	req := &pbmedia.GetFilesByMetadataRequest{
+		DesiredMetadata: fileMetadata,
+		Strictness:      pbmedia.MetadataStrictness_STRICT,
+	}
+
+	resp, err := m.mediaClient.GetFilesWithMetadata(ctx, req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.FileInfos, nil
+}
+
 type UserClientWrapper struct {
 	usersClient pbusers.UsersClient
 }
@@ -79,4 +111,53 @@ func NewUserClientWrapper(conn *grpc.ClientConn) *UserClientWrapper {
 	return &UserClientWrapper{
 		usersClient: pbusers.NewUsersClient(conn),
 	}
+}
+
+func (w *UserClientWrapper) GetUserNameForID(
+	ctx context.Context,
+	userID int64,
+	authCredentials string,
+) (string, error) {
+	md := metadata.Pairs("Authorization", authCredentials)
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	req := &pbusers.GetUserNameByIDRequest{UserID: userID}
+
+	resp, err := w.usersClient.GetUserNameByID(ctx, req)
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Username, nil
+}
+
+type HealthClientWrapper struct {
+	healthClient pbhealth.HealthTrackingClient
+}
+
+func NewHealthClientWrapper(conn *grpc.ClientConn) *HealthClientWrapper {
+	return &HealthClientWrapper{
+		healthClient: pbhealth.NewHealthTrackingClient(conn),
+	}
+}
+
+func (h *HealthClientWrapper) GetMentalHealthScoreForUser(
+	ctx context.Context,
+	userID int64,
+	authCredentials string,
+) (int32, error) {
+
+	md := metadata.Pairs("Authorization", authCredentials)
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	req := &pbhealth.GetMentalHealthScoreForUserRequest{UserID: userID}
+
+	resp, err := h.healthClient.GetMentalHealthScoreForUser(ctx, req)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return resp.Score, nil
 }
--- internal/server/interfaces.go
+++ internal/server/interfaces.go
@@ -1,6 +1,9 @@
 package server
 
-import "context"
+import (
+	"context"
+	pbcommon "github.com/kic/feed/pkg/proto/common"
+)
 
 type FriendServicer interface {
 	GetFriendsForUser(context.Context, int64, string) ([]uint64, error)
@@ -8,7 +11,13 @@ type FriendServicer interface {
 }
 
 type UserServicer interface {
+	GetUserNameForID(ctx context.Context, userID int64, authCredentials string) (string, error)
 }
 
 type MediaServicer interface {
+	GetFilesForUser(ctx context.Context, userID int64, authCredentials string) ([]*pbcommon.File, error)
+}
+
+type HealthServicer interface {
+	GetMentalHealthScoreForUser(ctx context.Context, userID int64, authCredentials string) (int32, error)
 }
